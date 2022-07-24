dd9371b4cdeb0234c6b3134426d85ec12d386393
cazdlt <cazdlt@gmail.com>
2022-07-23 22:56:35 -0500

    add topic subscription  logic

--- src/app/mqtt.py
+++ src/app/mqtt.py
@@ -2,69 +2,112 @@ import time
 
 import dht
 from machine import Pin
+from lib.mqtt_manager import MQTTManager
 from umqtt.simple import MQTTClient
-
-# Connection config
-MQTT_CLIENT_NAME = "esp"
-MQTT_BROKER_ADDR = "192.168.1.20"
-MQTT_KEEPALIVE = 60
 
 # Device config
 DEVICE_ID = 0
-SENSOR_PIN = 4
+DEVICE_NAME = f"esp{DEVICE_ID}"
 
-# topics
-esp_id = f"esp{DEVICE_ID}"
-topic_hum = f"{esp_id}/sensor/hum"
-topic_temp = f"{esp_id}/sensor/temp"
-topic_led = f"{esp_id}/actuator/led"
-topics_to_subscribe = []
+# Connection config
+MQTT_CLIENT_NAME = DEVICE_NAME
+MQTT_BROKER_ADDR = "192.168.1.20"
+MQTT_KEEPALIVE = 60
+
+# Topics
+topic_hum = f"{DEVICE_NAME}/sensor/hum"
+topic_temp = f"{DEVICE_NAME}/sensor/temp"
+topic_led = f"{DEVICE_NAME}/actuator/led"
+topic_health_check = f"{DEVICE_NAME}/health_check"
+topics_to_subscribe = [topic_led, topic_health_check]
+
+# Pins
+SENSOR_PIN_NUMBER = 4
+sensor_pin = Pin(SENSOR_PIN_NUMBER, Pin.IN, Pin.PULL_UP)
+led_pin = Pin(2, Pin.OUT)
 
 
-def callback(topic, msg):
-    print(topic)
-    print(msg)
+def led_callback(message: str):
+    if message == "on":
+        print("Turning on LED")
+        led_pin.on()
+    elif message == "off":
+        print("Turning off LED")
+        led_pin.off()
+    else:
+        print("Invalid message for led controller:", message)
 
 
-def publish_measurements(mqtt: MQTTClient) -> None:
+def health_check_callback(message: str):
+    print("Hola mamá")
 
-    print("Publishing sensor data to ", topic_hum, topic_temp)
 
-    pin = Pin(SENSOR_PIN, Pin.IN, Pin.PULL_UP)
-    sensor = dht.DHT11(pin)
-    sensor.measure()
+def callback(topic: bytes, message: bytes):
+    topic = topic.decode("utf-8")
+    message = message.decode("utf-8")
 
-    humidity = sensor.humidity()
-    temperature = sensor.temperature()
-    print("hum", humidity)
-    print("temp", temperature)
+    try:
+        topic_callbacks = {
+            topic_led: led_callback,
+            topic_health_check: health_check_callback,
+        }
+        topic_callbacks[topic](message)
+    except KeyError:
+        print("Unknown action for topic", topic)
+        print("Message:", message)
+
+
+def get_measurements(sensor: dht.DHT11):
+
+    try:
+        sensor.measure()
+        humidity = sensor.humidity()
+        temperature = sensor.temperature()
+    except Exception as e:
+        print("Error while reading sensor measurements", e)
+        humidity = temperature = -1
+    return humidity, temperature
+
+
+def publish_measurements(sensor: dht.DHT11, mqtt: MQTTClient) -> None:
+
+    print("Reading sensor data...")
+    humidity, temperature = get_measurements(sensor)
+
+    print("Sending humidity", humidity, "to", topic_hum)
     mqtt.publish(topic_hum, str(humidity))
+
+    print("Sending temperature", temperature, "to", topic_temp)
     mqtt.publish(topic_temp, str(temperature))
 
 
-def connect_mqtt() -> MQTTClient:
-    mqtt = MQTTClient(MQTT_CLIENT_NAME, MQTT_BROKER_ADDR, keepalive=MQTT_KEEPALIVE)
-
-    print("Connecting to broker...")
-    mqtt.connect()
-    print("Connected to broker.")
-
+def subscribe_to_topics(mqtt: MQTTClient, topics: list[str], callback):
     mqtt.set_callback(callback)
 
-    for topic in topics_to_subscribe:
+    for topic in topics:
+        print("Subscribing to", topic)
         mqtt.subscribe(topic)
 
-    return mqtt
+
+def initialize_sensor():
+
+    sensor = dht.DHT11(sensor_pin)
+    return sensor
 
 
 def main():
 
     print("Starting!!")
-    mqtt = connect_mqtt()
+    sensor = initialize_sensor()
 
-    while True:
-        time.sleep(1)
-        publish_measurements(mqtt)
+    with MQTTManager(MQTT_CLIENT_NAME, MQTT_BROKER_ADDR) as mqtt:
+        subscribe_to_topics(mqtt, topics_to_subscribe, callback)
+
+        while True:
+            print()
+            mqtt.check_msg()
+            publish_measurements(sensor, mqtt)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
