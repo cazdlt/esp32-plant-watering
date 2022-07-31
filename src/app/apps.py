4177c9d6a849986be6d2bace5ad7aedb2291d3f9
cazdlt <cazdlt@gmail.com>
2022-07-30 21:22:16 -0500

    add playground app

--- src/app/apps.py
+++ src/app/apps.py
@@ -1,7 +1,11 @@
-from . import home_assistant, mqtt
+from . import home_assistant, mqtt, playground
 
 
 def run_app(app: str):
-    apps = {"mqtt": mqtt.main, "ha": home_assistant.main}
+    apps = {
+        "mqtt": mqtt.main,
+        "ha": home_assistant.main,
+        "playground": playground.main,
+    }
     assert app in apps.keys(), f"{app} is not a valid app"
     return apps[app]()
---
+++ src/app/playground.py
@@ -0,0 +1,7 @@
+def main():
+    a="doña".encode("utf-8")
+    print(a)
+    print(a.decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
